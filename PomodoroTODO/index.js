e2a51ef42413edb761964910169662095debc8fd
Eb3ls <ebelord32@gmail.com>
2024-05-06 17:58:43 +0200

    Pagine di login e signup fatte a livello estetico

--- PomodoroTODO/index.js
+++ PomodoroTODO/index.js
@@ -13,8 +13,26 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.get('/', async function(req, res){
-    res.render('home');  
+    res.render('login');  
+})
+
+app.get('/signup', async function(req, res){
+    res.render('signup');  
+})
+
+app.post('/login', async function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    //GET USER FROM DATABASE AND CHECK IF PASSWORD MATCHES OR IF USER EXISTS
+})
+
+app.post('/register', async function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    //CREATE USER IN DATABASE IF IT DOESN'T EXIST
 })
 
 
